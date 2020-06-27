2034f502cca79ac5078f354ea99ed37f49c5b1ae
jimo <jimolonely@yahoo.com>
2020-06-27 17:39:59 +0800

    利润表

--- com/jimo/data/load_report.py
+++ com/jimo/data/load_report.py
@@ -1,10 +1,14 @@
 import requests
+import xlwt
+import json
+from collections import OrderedDict
 
 
 class LoadReport(object):
 
-    def __init__(self, code):
+    def __init__(self, code, last_year):
         self.code = code
+        self.last_year = last_year + 1
 
     def req_data(self):
         """
@@ -23,10 +27,43 @@ class LoadReport(object):
               '&timestamp='.format(self.code)
         header = self.get_header()
         res = requests.get(url, headers=header)
-        return res.json()['data']
+        return res.json()['data']['list']
 
     def write_excel(self):
-        pass
+        wb = xlwt.Workbook(encoding='utf-8')
+        sheet = wb.add_sheet(sheetname='利润表', cell_overwrite_ok=True)
+        self.write_sheet(sheet, '利润表术语对应表.json')
+        wb.save('{}财报.xlsx'.format(self.code))
+
+    def write_sheet(self, sheet, item_map_name):
+        data = self.req_data()
+        sheet.write(0, 0, self.code)
+        i = 1
+        from_year = self.last_year - len(data) + 1
+        for y in range(from_year, self.last_year):
+            sheet.write(0, i, y)
+            i = i + 1
+        i = 1
+        keys = []
+        # 写第一列的项目名，需要中英文映射
+        name_map = {}
+        with open(item_map_name, 'r', encoding='utf-8') as f:
+            name_map = json.load(f, object_pairs_hook=OrderedDict)
+        for k in name_map.keys():
+            sheet.write(i, 0, name_map[k])
+            keys.append(k)
+            i = i + 1
+        j = 1
+        for one_year in data:
+            i = 1
+            for k in keys:
+                d = one_year[k]
+                if isinstance(d, list) and d[0]:
+                    sheet.write(i, j, str(d[0]))
+                else:
+                    sheet.write(i, j, '0')
+                i += 1
+            j += 1
 
     @staticmethod
     def get_header():
@@ -37,13 +74,14 @@ class LoadReport(object):
                   'Sec-Fetch-Mode': 'cors', 'Sec-Fetch-Site': 'same-site',
                   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                                 'Chrome/83.0.4103.61 Safari/537.36',
-                  'Cookie': 'device_id=24700f9f1986800ab4fcc880530dd0ed; s=cs1cqgondz; xq_a_token=ea139be840cf88ff8c30e6943cf26aba8ad77358; xqat=ea139be840cf88ff8c30e6943cf26aba8ad77358; xq_r_token=863970f9d67d944596be27965d13c6929b5264fe; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTU5NDAwMjgwOCwiY3RtIjoxNTkxNTMxODQyMjM1LCJjaWQiOiJkOWQwbjRBWnVwIn0.o189WRRzRJQDSnP8USNyktdNaz8yhPjm9rfvZtXHymOz8IoQm0EPetmfo4Psn7L3x5Zqroo-_p8sITOiuqvi32iIuIUkR6ZpitpaBhWywJPdV836iiIrlJ3lThIYlqrtkzgqyhhq5A6t6XYszGezYjrFdpwNgjPbrnD3OdvVb5zHTIwfR9O80_8HxM60NRygoBgam_UZqhGurrN8nzEO9nDyYGPnAHnxDRAz4kgbZWGfklLWFijqLLQaFvnN_0jSMQhO_J0QNpr4SJ2O6kkCuyanHHR28xotFJs-r_awAkNEdzkvdOu-5HdmzSwqDjLQSBlQvwE3jgBCAgCUU6BSNA; u=941591531846409; Hm_lvt_1db88642e346389874251b5a1eded6e3=1591531849; is_overseas=0; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1592123351'
+                  'Cookie': 'device_id=24700f9f1986800ab4fcc880530dd0ed; s=cs1cqgondz; xq_a_token=ea139be840cf88ff8c30e6943cf26aba8ad77358; xqat=ea139be840cf88ff8c30e6943cf26aba8ad77358; xq_r_token=863970f9d67d944596be27965d13c6929b5264fe; xq_id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1aWQiOi0xLCJpc3MiOiJ1YyIsImV4cCI6MTU5NDAwMjgwOCwiY3RtIjoxNTkxNTMxODQyMjM1LCJjaWQiOiJkOWQwbjRBWnVwIn0.o189WRRzRJQDSnP8USNyktdNaz8yhPjm9rfvZtXHymOz8IoQm0EPetmfo4Psn7L3x5Zqroo-_p8sITOiuqvi32iIuIUkR6ZpitpaBhWywJPdV836iiIrlJ3lThIYlqrtkzgqyhhq5A6t6XYszGezYjrFdpwNgjPbrnD3OdvVb5zHTIwfR9O80_8HxM60NRygoBgam_UZqhGurrN8nzEO9nDyYGPnAHnxDRAz4kgbZWGfklLWFijqLLQaFvnN_0jSMQhO_J0QNpr4SJ2O6kkCuyanHHR28xotFJs-r_awAkNEdzkvdOu-5HdmzSwqDjLQSBlQvwE3jgBCAgCUU6BSNA; u=941591531846409; Hm_lvt_1db88642e346389874251b5a1eded6e3=1591531849; is_overseas=0; Hm_lpvt_1db88642e346389874251b5a1eded6e3=1593244768'
                   }
         return header
 
 
 if __name__ == '__main__':
-    r = LoadReport('SZ000895')
-    j = r.req_data()['list'][0]
-    for k in sorted(j.keys()):
-        print('"{}":"",'.format(k))
+    r = LoadReport('SZ000895', 2019)
+    r.write_excel()
+    # j = r.req_data()['list'][0]
+    # for k in sorted(j.keys()):
+    #     print('"{}":"",'.format(k))
